c90011458eb580c4b2e01014fdf055d8779d2251
Ionică Bizău <bizauionica@gmail.com>
2025-06-16 21:58:13 +0300

    :arrow_up: 1.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,16 @@
 
 const sequelizeIterate = require("../lib");
 
-console.log(sequelizeIterate());
+
+(async () => {
+    await SequelizeIterate({
+        model: User,
+        batch_size: 10,
+        onItem: async user => {
+            await doSomethingWithTheUser(user)
+        }
+        onBatch: async batchOfUsers => {
+            await doSomethingWithTheBatchOfUsers(batchOfUsers)
+        }
+    })
+})()
